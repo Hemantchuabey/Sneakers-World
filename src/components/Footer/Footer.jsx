af44f1842833813f451909192714217672ce19dd
Hemant <hementchaubey@gmail.com>
2023-08-02 12:20:14 +0530

    Made footer section mobile responsive.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,11 +8,11 @@ const Footer = () => {
       <div className='flex items-center justify-center'>
           <hr className='h-px w-4/5 bg-gray-400 outline-none border-none'/>
       </div>
-      <div className='flex items-center justify-around'>
+      <div className='flex items-center justify-around mt-6'>
         <div>
-          <img className='h-12' src={logo} alt='logo'></img>
+          <img className='w-32 h-8  md:w-52 md:h-10' src={logo} alt='logo'></img>
         </div>
-        <p className='text-black text-sm font-inter no-underline normal-case'>
+        <p className='text-black text-[12px] md:text-[1rem] font-inter no-underline normal-case'>
           Copyright {year} page by Hemant 
         </p>
       </div>
